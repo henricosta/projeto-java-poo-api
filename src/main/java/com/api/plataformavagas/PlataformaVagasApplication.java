f9d16676213a5cc7f4b668fd8b82ded643ed21fc
henricosta <pedrocosta.lov@gmail.com>
2023-11-16 18:16:54 -0300

    initial commit

---
+++ src/main/java/com/api/plataformavagas/PlataformaVagasApplication.java
@@ -0,0 +1,13 @@
+package com.api.plataformavagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaVagasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaVagasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/plataformavagas/PlataformaVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.plataformavagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaVagasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
