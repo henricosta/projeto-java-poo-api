424498869b22bb99f4e0bbdd4664500e38079192
henricosta <pedrocosta.lov@gmail.com>
2023-11-16 19:34:00 -0300

    create Education model and add one to may relationship in Candidate model

--- src/main/java/com/api/plataformavagas/models/Candidate.java
+++ src/main/java/com/api/plataformavagas/models/Candidate.java
@@ -2,6 +2,7 @@ package com.api.plataformavagas.models;
 
 import jakarta.persistence.*;
 
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +18,9 @@ public class Candidate {
     private String email;
 
     private String phone;
+
+    @OneToMany
+    private List<Education> educationList;
 
     public int getId() {
         return id;
---
+++ src/main/java/com/api/plataformavagas/models/Education.java
@@ -0,0 +1,79 @@
+package com.api.plataformavagas.models;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Education {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String institution;
+
+    @Column
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "candidate_id")
+    private Candidate candidate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Education education = (Education) o;
+        return id == education.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Candidate getCandidate() {
+        return candidate;
+    }
+
+    public void setCandidate(Candidate candidate) {
+        this.candidate = candidate;
+    }
+}
