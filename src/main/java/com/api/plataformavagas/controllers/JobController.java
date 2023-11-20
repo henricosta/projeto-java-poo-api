4a020aa61c61452de09ec718d6473dfa4c86b3aa
henricosta <pedrocosta.lov@gmail.com>
2023-11-20 17:06:53 -0300

    add create, list methods

--- src/main/java/com/api/plataformavagas/controllers/JobController.java
+++ src/main/java/com/api/plataformavagas/controllers/JobController.java
@@ -1,21 +1,25 @@
 package com.api.plataformavagas.controllers;
 
+import com.api.plataformavagas.models.Company;
 import com.api.plataformavagas.models.Job;
+import com.api.plataformavagas.repositories.CompanyRepository;
 import com.api.plataformavagas.repositories.JobRepository;
+import com.api.plataformavagas.requests.CreateJobRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/jobs")
 public class JobController {
     private final JobRepository jobRepository;
+    private final CompanyRepository companyRepository;
 
-    public JobController(JobRepository jobRepository) {
+    public JobController(JobRepository jobRepository, CompanyRepository companyRepository) {
         this.jobRepository = jobRepository;
+        this.companyRepository = companyRepository;
     }
 
     @GetMapping("/details")
@@ -29,4 +33,54 @@ public class JobController {
         return new ResponseEntity<>(j, HttpStatus.OK);
     }
 
+    @GetMapping("/list")
+    public ResponseEntity<Page<Job>> listJobs(
+            @RequestParam(name = "title", required = false) String title,
+            @RequestParam(name = "type", required = false) String type,
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size) {
+
+        PageRequest pageable = PageRequest.of(page, size);
+        Page<Job> resultPage;
+
+        try {
+            if (title != null && type != null) {
+                resultPage = this.jobRepository.findByTitleIgnoreCaseContainingAndType(title, type, pageable);
+            } else if (title != null) {
+                resultPage = this.jobRepository.findByTitleIgnoreCaseContaining(title, pageable);
+            } else if (type != null) {
+                resultPage = this.jobRepository.findByType(type, pageable);
+            } else {
+                resultPage = this.jobRepository.findAll(pageable);
+            }
+
+            return new ResponseEntity<>(resultPage, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createJob(@RequestBody CreateJobRequest request) {
+        Company c = this.companyRepository.findById(request.getCompany_id()).orElse(null);
+        if (c == null) {
+            return new ResponseEntity<>("Company does not exist", HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            Job j = new Job();
+            j.setTitle(request.getTitle());
+            j.setDescription(request.getDescription());
+            j.setType(request.getType());
+            j.setCompany(c);
+
+            this.jobRepository.save(j);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 }
--- src/main/java/com/api/plataformavagas/repositories/JobRepository.java
+++ src/main/java/com/api/plataformavagas/repositories/JobRepository.java
@@ -1,7 +1,14 @@
 package com.api.plataformavagas.repositories;
 
 import com.api.plataformavagas.models.Job;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobRepository extends JpaRepository<Job, Integer> {
+    Page<Job> findByTitleIgnoreCaseContainingAndType(String search, String type, Pageable pageable);
+
+    Page<Job> findByTitleIgnoreCaseContaining(String search, Pageable pageable);
+
+    Page<Job> findByType(String type, Pageable pageable);
 }
