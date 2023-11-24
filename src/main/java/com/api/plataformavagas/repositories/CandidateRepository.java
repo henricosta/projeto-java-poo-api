ca2619be1bfc16e4f89ce71d4504fa9b16ac133b
henricosta <pedrocosta.lov@gmail.com>
2023-11-24 14:53:44 -0300

    make find by email return a UserDetails interface

--- src/main/java/com/api/plataformavagas/repositories/CandidateRepository.java
+++ src/main/java/com/api/plataformavagas/repositories/CandidateRepository.java
@@ -2,6 +2,8 @@ package com.api.plataformavagas.repositories;
 
 import com.api.plataformavagas.models.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
+    UserDetails findByEmail(String email);
 }
