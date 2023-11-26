211026ed65ee50162de768f5fcc5a8948dca8872
henricosta <pedrocosta.lov@gmail.com>
2023-11-26 10:27:41 -0300

    add Company to authorization service

--- src/main/java/com/api/plataformavagas/services/AuthorizationService.java
+++ src/main/java/com/api/plataformavagas/services/AuthorizationService.java
@@ -7,14 +7,24 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.api.plataformavagas.repositories.CandidateRepository;
+import com.api.plataformavagas.repositories.CompanyRepository;
 
 @Service
 public class AuthorizationService implements UserDetailsService {
     @Autowired
     CandidateRepository candidateRepository;
+    @Autowired
+    CompanyRepository companyRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return candidateRepository.findByEmail(username);
+
+        UserDetails candidateUser = candidateRepository.findByEmail(username);
+
+        if (candidateUser != null) {
+            return candidateUser;
+        }
+
+        return companyRepository.findByEmail(username);
     }
 }
