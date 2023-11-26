af7f71cd958dbbabccdae1dfd54684307eca7da3
henricosta <pedrocosta.lov@gmail.com>
2023-11-26 13:37:24 -0300

    create record to handle requests to create jobs

--- src/main/java/com/api/plataformavagas/requests/CreateJobRequest.java
+++ src/main/java/com/api/plataformavagas/requests/CreateJobRequest.java
@@ -1,40 +1,5 @@
 package com.api.plataformavagas.requests;
 
-public class CreateJobRequest {
-    private String title;
-    private String description;
-    private String type;
-    private int company_id;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public int getCompany_id() {
-        return company_id;
-    }
-
-    public void setCompany_id(int company_id) {
-        this.company_id = company_id;
-    }
+public record CreateJobRequest(String title, String description, String type, int company_id) {
+    
 }
