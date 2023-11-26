0bf18b96c05225427a07b61944ca44cfeb3fabe4
henricosta <pedrocosta.lov@gmail.com>
2023-11-26 08:41:13 -0300

    add findByEmail to user repository
    returns a UserDetails to be used in authentication

--- src/main/java/com/api/plataformavagas/repositories/CompanyRepository.java
+++ src/main/java/com/api/plataformavagas/repositories/CompanyRepository.java
@@ -2,6 +2,8 @@ package com.api.plataformavagas.repositories;
 
 import com.api.plataformavagas.models.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
+    UserDetails findByEmail (String email);
 }
