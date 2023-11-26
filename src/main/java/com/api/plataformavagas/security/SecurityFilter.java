e9e8fd71883c3b97a2fee9508c28d47e0b1c1e86
henricosta <pedrocosta.lov@gmail.com>
2023-11-26 13:36:46 -0300

    make filter handle both company and candidate

--- src/main/java/com/api/plataformavagas/security/SecurityFilter.java
+++ src/main/java/com/api/plataformavagas/security/SecurityFilter.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.api.plataformavagas.repositories.CandidateRepository;
+import com.api.plataformavagas.repositories.CompanyRepository;
 import com.api.plataformavagas.services.TokenService;
 
 import java.io.IOException;
@@ -22,13 +23,20 @@ public class SecurityFilter extends OncePerRequestFilter {
     TokenService tokenService;
     @Autowired
     CandidateRepository candidateRepository;
+    @Autowired
+    CompanyRepository companyRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
         if(token != null){
             var email = tokenService.validateToken(token);
+        
             UserDetails user = candidateRepository.findByEmail(email);
+
+            if (user == null) {
+                user = companyRepository.findByEmail(email);
+            }
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
